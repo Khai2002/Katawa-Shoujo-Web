e477e04b225c7b832831756aa8fd57f945454c29
Khai2002 <khainguyenletuan@gmail.com>
2023-03-24 09:00:43 +0100

    changing abit

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -10,12 +10,12 @@ function Contact() {
         <p>If you have any inquiries, contact me at (most active on Reddit)</p>
         
         <div className='flex-box-button'>
-            <a href='https://www.patreon.com/KyleDaBoss'><button className='button-choice-low'><FontAwesomeIcon icon={faReddit} />  Reddit</button></a>
-            <a href='https://www.patreon.com/KyleDaBoss'><button className='button-choice-low'><FontAwesomeIcon icon={faDiscord} />  Discord</button></a>
+            <a href='https://www.reddit.com/user/hakhoi123'><button className='button-choice-low'><FontAwesomeIcon icon={faReddit} />  Reddit</button></a>
+            <a href='https://discord.gg/fd7NBVRWhq'><button className='button-choice-low'><FontAwesomeIcon icon={faDiscord} />  Discord</button></a>
             <a href='https://www.patreon.com/KyleDaBoss'><button className='button-choice-low'><FontAwesomeIcon icon={faYoutube} />  Youtube</button></a>
         </div>
         <br/>
-        <p>Or send an email to: def@gmail.com</p>
+        <p>Or send an email to: kyledabossks@gmail.com</p>
     </div>;
 }
 
